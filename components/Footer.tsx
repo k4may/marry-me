900e4b0512cfbe324c4c6706bcf8d313881b27c9
Kalebe <kalebemaynard@gmail.com>
2025-03-15 18:10:17 -0300

    fix: corrigido erro de navegação no Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,25 @@
+import Link from "next/link";
 import "../src/app/globals.css";
 
 const Footer = () => {
-    return (
-      <footer className=" text-gray-400 text-center py-6 mt-10">
-        <div className="container mx-auto px-4">
-          <p className="text-sm">
-            &copy; {new Date().getFullYear()} Casamento de Kalebe e Shelda. Todos os direitos reservados.
-          </p>
-          <div className="mt-2 space-x-4">
-            <a href="/termos-de-uso" className="hover:text-white transition">
-              Termos de Uso
-            </a>
-            <span>|</span>
-            <a href="/politica-de-privacidade" className="hover:text-white transition">
-              Política de Privacidade
-            </a>
-          </div>
+  return (
+    <footer className="text-gray-400 text-center py-6 mt-10">
+      <div className="container mx-auto px-4">
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} Casamento de Kalebe e Shelda. Todos os direitos reservados.
+        </p>
+        <div className="mt-2 space-x-4">
+          <Link href="/termos-de-uso" className="hover:text-white transition">
+            Termos de Uso
+          </Link>
+          <span>|</span>
+          <Link href="/politica-de-privacidade" className="hover:text-white transition">
+            Política de Privacidade
+          </Link>
         </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
